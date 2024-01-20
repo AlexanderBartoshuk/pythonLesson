3b102f7cee911914621ccc51ae50ed55860e49e0
Alexandr Bartoshuk <sasha_bartoshuk02@mail.ru>
2024-01-20 16:06:35 +0300

    FibonacciTask

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+def Fibonachi(n):
+    if n <= 0:
+        input = int(input("Введите новое число:"))
+        Fibonachi(input)
+    else:
+        if n == 1:
+            return 0
+        else:
+            if n == 2:
+                return 1
+            else:
+                return Fibonachi(n-1) + Fibonachi(n-2)
+
+
+input = int(input("Введите число:"))
+print(Fibonachi(input))
