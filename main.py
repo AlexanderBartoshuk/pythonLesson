c6b4f92a448baec07305b05a6def1c646b8ff630
m5hut <m5555hutov@gmail.com>
2024-01-27 15:28:17 +0300

    Lesson 2 Task 1, Loops

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
+#Implement a program that finds the sum of the digits of a given number
 
-def homework(n):
-    if n == 3:
-        return "True"
-    else:
-        if n % 3 != 0:
-            return "False"
-        else:
-             return homework(n // 3)
-
-input = int(input("Введите число:"))
-
-print(homework(input))
 
