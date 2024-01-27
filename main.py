1ad5e31e3167016893c6f9a82c329bdb87b5c007
m5hut <m5555hutov@gmail.com>
2024-01-27 15:45:58 +0300

    Lesson 2 Task 1, Loops, SOLVED

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #Implement a program that finds the sum of the digits of a given number
 
+n = int(input("Введите число:"))
+list = []
+while n > 0 :
+    list.insert(0, n % 10)
+    n = n // 10
+
+print(list)
 
