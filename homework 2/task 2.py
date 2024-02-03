e1019ad18d500dac1c12e5b85c4ff9a61ad2c251
AlexandrBartoshuk <sasha_bartoshuk02@mail.ru>
2024-02-03 15:53:26 +0300

    Homework 2 refactor

--- homework 2/task 2.py
+++ homework 2/task 2.py
@@ -1,11 +1,14 @@
 number = int(input("Введите число:"))
 
 sum = 0
-count = 1
+count = 2
 
 while count <= number:
-    if count % 1 == 0:
+    if count % 2 == 0:
         sum = sum + count
+        count = count - 2
+    else:
         count = count + 1
+
 print(sum)
 
--- task 3.py
+++
@@ -1,7 +0,0 @@
-def homework(n):
-    if n < 10:
-        return n
-    else:
-        return n % 10 + homework(n // 10)
-input = int(input("Введите число:"))
-print(homework(input))
