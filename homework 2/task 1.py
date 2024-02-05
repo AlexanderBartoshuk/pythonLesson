c409d6429ea76e7617882e3c4728ae152d0b6016
AlexandrBartoshuk <sasha_bartoshuk02@mail.ru>
2024-02-05 17:10:09 +0300

    Homework 2 task 1

--- homework 2/task 1.py
+++ homework 2/task 1.py
@@ -1,5 +1,13 @@
-a = int(input("Введите 1 число:"))
-b = int(input("Введите 2 число:"))
-
-for x in range(a,b+1):
-    print(x)
+def work(x, y):
+    work_list = []
+    for i in range(x, y):
+        if i == 0 or i == 1:
+            continue
+        else:
+            for a in range(2, int(i/2)+1):
+                if i % a == 0:
+                    break
+            else:
+                work_list.append(i)
+    return work_list
+print(work(1,746))
