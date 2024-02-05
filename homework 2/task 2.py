e51a8a765c8214af406c1027f8cfb953b790ad24
AlexandrBartoshuk <sasha_bartoshuk02@mail.ru>
2024-02-05 16:57:58 +0300

    Homework 2 task 2

--- homework 2/task 2.py
+++ homework 2/task 2.py
@@ -1,12 +1,12 @@
-number = int(input("Введите число:"))
+n = int(input("Введите число:"))
 
 sum = 0
 count = 2
 
-while count <= number:
+while count <= n:
     if count % 2 == 0:
         sum = sum + count
-        count = count - 2
+        count = count + 2
     else:
         count = count + 1
 
