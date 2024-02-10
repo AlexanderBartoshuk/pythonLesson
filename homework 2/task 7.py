9cefe980bb2cc8ed3c6b6d563a98ddb0686865c6
m5hut <m5555hutov@gmail.com>
2024-02-10 12:12:46 +0300

    Refactor homerwork2 task 7

--- homework 2/task 7.py
+++ homework 2/task 7.py
@@ -7,10 +7,24 @@ def calculator(num1,num2,action):
         return num1 * num2
     elif action == "/":
         return num1 / num2
+    else:
+        print("incorrect input!!, please try again:")
+        userInput()
 
-num1 = float(input("The first count is: "))
-num2 = float(input("The second count is: "))
-action = input("Choose the action: (+ ; - ; * ; / ) ")
 
-result = calculator(num1,num2,action)
-print(result)
+
+def userInput():
+    try:
+        num1 = float(input("The first count is: "))
+        num2 = float(input("The second count is: "))
+        action = input("Choose the action: (+ ; - ; * ; / ) ")
+        print("result:", calculator(num1, num2, action))
+    except:
+        print("Incorrect number format, please try again")
+        userInput()
+
+
+
+
+
+userInput()
