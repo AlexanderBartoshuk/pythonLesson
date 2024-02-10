ebdbd89fb52a141fa337e9894428b7835870d059
m5hut <m5555hutov@gmail.com>
2024-02-10 13:03:04 +0300

    Refactor task 7

--- homework 2/task 7.py
+++ homework 2/task 7.py
@@ -1,30 +1,23 @@
-def calculator(num1,num2,action):
-    if action == "+":
-        return num1 + num2
-    elif action == "-":
-        return num1-num2
-    elif action == "*":
-        return num1 * num2
-    elif action == "/":
-        return num1 / num2
-    else:
-        print("incorrect input!!, please try again:")
-        userInput()
+def task7():
+    def userInput():
+        try:
+            num1 = float(input("The first count is: "))
+            num2 = float(input("The second count is: "))
+            action = input("Choose the action: (+ ; - ; * ; / ) ")
+            print("result:", calculator(num1, num2, action))
+        except:
+            print("Incorrect number format, please try again")
+            userInput()
 
-
-
-def userInput():
-    try:
-        num1 = float(input("The first count is: "))
-        num2 = float(input("The second count is: "))
-        action = input("Choose the action: (+ ; - ; * ; / ) ")
-        print("result:", calculator(num1, num2, action))
-    except:
-        print("Incorrect number format, please try again")
-        userInput()
-
-
-
-
-
-userInput()
+    def calculator(num1, num2, action):
+        if action == "+":
+            return num1 + num2
+        elif action == "-":
+            return num1 - num2
+        elif action == "*":
+            return num1 * num2
+        elif action == "/":
+            return num1 / num2
+        else:
+            print("incorrect input!!, please try again:")
+            userInput()
