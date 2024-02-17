44161e0b3770f9079065bae50481e4f21d19a0fd
AlexandrBartoshuk <sasha_bartoshuk02@mail.ru>
2024-02-17 15:00:34 +0300

    homework 2 task 2

--- homework/task 8.py
+++ homework/task 8.py
@@ -1,4 +1,13 @@
-x = [2,53,832,-342,452,-53.5,64]
+list = [2, 53, 832, -342, 452, -53.5, 64]
 
-a = lambda x: sorted(x)
-print(a(x))
+lam_sort = lambda list: sorted(list)
+lam_lenght = lambda list: len(list)
+lam_sort_reverse = lambda list: sorted(list,reverse=True)
+def sor(list,func):
+    return func(list)
+
+print(sor(list,lam_sort))
+print(sor(list, lam_lenght))
+print(sor(list, lam_sort_reverse))
+
+print(sor(list, lambda list: sorted(list)))
