daba65b1e4333ecbe76edc4a54e5491944608dab
AlexandrBartoshuk <sasha_bartoshuk02@mail.ru>
2024-03-12 21:53:51 +0300

    homework 5 task 1

---
+++ homework/task 17.py
@@ -0,0 +1,21 @@
+class Person():
+
+    def __init__(self,eat):
+        self.eat = eat
+    def walk(self):
+        print("Немного походим)")
+
+    def run(self):
+        print("Тебе стоит побегать")
+
+    def sleep(self):
+        print("Пора спать")
+
+    def eat(self):
+        print("Пора кушать")
+
+    def study(self):
+        print("Немного поучимся")
+
+    def work(self):
+        print("Пока немного поработать")
