234423214b0360b6cc12898abb89e3525f685b80
m5hut <m5555hutov@gmail.com>
2024-03-23 12:27:49 +0300

    commit mrg

--- homework/task 18.py
+++ homework/task 18.py
@@ -1,64 +1,46 @@
 class Orc:
 
     def __init__(self,power,intelect,dexterity,health,endurance,level,experience):
-    def __init__(self,power,intelect,dexterity):
         self.power = power
         self.intelect = intelect
         self.dexterity = dexterity
-        self.health = health
-        self.endurance = endurance
-        self.level = level
-        self.experience = experience
-        self.health = 54
-        self.endurance = 100
-        self.level = 1
-        self.experience = 0
-        self.use_weapon = "Машет дубинкой"
+        self.health = health # Задавать колличество xp не через параметры
+        self.endurance = endurance # задавать endurance не через параметры
+        self.level = level # Задавать lvl не через параметры
+        self.experience = experience  # задавать experience не через параметры
 
     def walk(self):
         print("Вперед")
 
+    def back(self):
+        print("Назад")
+
+    def left(self):
+        print("Влево")
+
     def right(self):
         print("Прямо")
 
+    # ошибка в логике, если у нас выносливости недостаточно для махания дубиной - нечего ей махать, выводи, что недостаточно выносливости
     def mahat_dubinloi(self):
         if self.endurance > 0:
-    def use_weapon(self):
-        print("Машет дубинкой")
-    def mahat_dubinkoi(self):
-        if self.endurance >= 10:
             self.endurance -= 10
-            print(f"Махал дубинкой, {self.endurance}")
-            print(f"Выносливость = {self.endurance}, Здоровье = {self.health}")
+            print(f"Махал дубинкой, {self.endurance}") # Махал дубиной, current endurance: self.endurance
         else:
             self.health -= 10
             print(f"Махал дубинкой, {self.health}")
-            tmp = 10 - self.endurance
-            self.health -= tmp
-            self.endurance = 0
-            if self.health <= 0:
-                self.die()
-            else:
-                print(f"Выносливость = {self.endurance}, Здоровье = {self.health}")
 
+    # рекурсия
     def treated(self):
         if self.treated() < self.health:
             self.treated += 10
             print(f'Лечился {self.treated}')
-        max = 100
-        if self.health < max:
-            self.health += 10
-            if self.health >= max:
-                self.health = max
-            print(f"Лечился, {self.health}")
         else:
-            self.health = 100
             print("Вы уже здоровы")
 
     def upgrade_level(self):
         self.level += 1
-        self.experience += 0
-        self.experience = 0
+        self.experience += 0 # зне имеет смысла
         print(f"Апгрейд уровня {self.level}")
 
     def die(self):
@@ -66,20 +48,4 @@ class Orc:
 
 orc = Orc(15,5,5,34,3,0,42)
 
-class Elif(Orc):
-
-    def init(self,power,intelect,dexterity,use_weapon):
-        super().__init__(power,intelect,dexterity)
-        self.use_weapon() == use_weapon
-
-    def use_weapon(self):
-        super().use_weapon()
-        print(f"Action = Стреять из лука")
-
-class Person(Orc):
-
 orc.mahat_dubinloi()
-    def __init__(self,power,intelect,dexterity):
-
-        super().__init__(power,intelect,dexterity)
-
