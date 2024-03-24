ce4dc4d281d1e5d9122bdc1613295102c74ffc3e
AlexandrBartoshuk <sasha_bartoshuk02@mail.ru>
2024-03-24 16:26:53 +0300

    homework 7 task 1

--- homework/task 18.py
+++ homework/task 18.py
@@ -1,6 +1,6 @@
 class Person():
 
-    def __init__(self,tamina : int, name: str, health: int, intellect: int, power: int, dexterity: int, experience: int, level: int):
+    def __init__(self,tamina : int, name: str, health: int,treat: int, intellect: int, power: int, dexterity: int, experience: int, level: int, max_health: int ):
         self.name = name
         self.health = health
         self.intellect = intellect
@@ -9,6 +9,8 @@ class Person():
         self.tamina = experience
         self.level = level
         self.tamin = tamina
+        self.treat = treat
+        self.max_health = max_health
     def go_direct(self):
         print(self.name, "идет прямо 10 шагов")
     def go_back(self):
@@ -32,19 +34,20 @@ class Person():
                 self.die()
             else:
                 print(f"Выносливость = {self.tamina}, Здоровье = {self.health}")
+
     def treated(self):
-        if self.treated() < self.health:
-            self.treated += 10
-            print(f'Лечился {self.treated}')
-        max = 100
-        if self.health < max:
+        if self.treat < self.health:
+            self.treat = 10
+
+        if self.health < self.max_health:
             self.health += 10
-            if self.health >= max:
-                self.health = max
-            print(f"Лечился, {self.health}")
+            print(f"Лечился на {self.treat},текущее здоровье - {self.health}")
+            if self.health >= self.max_health:
+                self.health = self.max_health
         else:
-            self.health = 100
-            print("Вы уже здоровы")
+            self.health = self.max_health
+            print("Лечение не требуется, вы уже здоровы")
+
     def upgrade_level(self):
         self.level += 1
         self.tamina += 0
@@ -53,48 +56,51 @@ class Person():
     def die(self):
         print("Смерть")
 
+
+
+
 class Orc(Person):
 
-    def __init__(self, name: str):
+     def __init__(self, name: str):
 
-        super().__init__(100,name,15, 65,20, 0, 1,0)
-    def za_ordu(self):
-        print("За Орду!")
-        self.die()
+         super().__init__(100,name,15, 65,20, 0, 1,0,1,132)
+     def za_ordu(self):
+         print("За Орду!")
+         self.die()
 
-    def use_weapon(self): super().use_weapon("Махать дубиной")
+     def use_weapon(self): super().use_weapon("Махать дубиной")
 
 class Human(Person):
 
-    def __init__(self, name: str):
+     def __init__(self, name: str):
 
-        super().__init__(100, name,70, 14,16, 0, 1,0)
+         super().__init__(100, name,70, 14,16, 0, 1,0,1,100)
 
-    def za_alians(self):
-       print("За Альянс")
-       self.die()
+     def za_alians(self):
+        print("За Альянс")
+        self.die()
 
-    def use_weapon(self):
-        super().use_weapon("Махать мечем")
+     def use_weapon(self):
+         super().use_weapon("Махать мечем")
 
 class Elif(Person):
 
-    def __init__(self, name: str):
-        super().__init__(100,name,150, 100, 20,30, 0, 0, )
+     def __init__(self, name: str):
+         super().__init__(90,name,150, 10, 10,30, 90, 100,1,100)
 
 
-    def za_loreron(self):
-        print("За Лордерон")
+     def za_loreron(self):
+         print("За Лордерон")
 
-    def use_weapon(self):
-        super().use_weapon("За Лордерон")
+     def use_weapon(self):
+         super().use_weapon("За Лордерон")
 
 orc = Orc("Вася")
 human = Human("Петя")
 elf = Elif("Нарамакил")
+per = Person(90,"Антон",100, 10, 10,30, 90, 100,1,100)
+for i in range(0,15):
+    elf.use_weapon()
 
-orc.go_left()
-human.go_direct()
-elf.za_loreron()
-elf.upgrade_level()
-elf.use_weapon()
+for i in range(0,15):
+    elf.treated()
