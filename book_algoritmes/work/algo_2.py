3c28666e108e127119ab4873f4ba1846275be002
AlexandrBartoshuk <sasha_bartoshuk02@mail.ru>
2024-07-19 23:08:31 +0300

    lesson_2

--- book_algoritmes/work/algo_2.py
+++ book_algoritmes/work/algo_2.py
@@ -1,10 +1,17 @@
-def sort(arr):
-    sortest = arr[0]
-    sortest_index = 0
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
     for i in range(1, len(arr)):
-        if arr[i] < sortest:
-            sortest = arr[i]
-            sortest_index = arr
-        return sortest_index
-    
-print(sort([1,4,6,3,0,10]))
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr) 
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectSort([1,23,45567,34,21,5]))
