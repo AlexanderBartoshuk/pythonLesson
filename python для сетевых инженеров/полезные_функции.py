1cc4f7dd96caebb91fc56bb98575e1fe86fe236c
AlexandrBartoshuk <sasha_bartoshuk02@mail.ru>
2024-09-22 13:20:05 +0300

    22_september

--- python для сетевых инженеров/полезные_функции.py
+++ python для сетевых инженеров/полезные_функции.py
@@ -79,3 +79,77 @@ def ignore_command(command):
     return False
 
 ignore_command("alias")
+
+#                лямда-функция 
+
+def sum_arg(a,b): return a + b
+print(sum_arg(5,2)) # - обычная функция 
+
+# лямда фнкция 
+sum = lambda a,b: a - b
+sum(8,4)
+
+# лямда функция для сортировки 
+
+list_of_tuple = [('IT_VLAN', 320),
+                 ('Mngmt_VLAN', 99),
+                 ('User_VLAN', 1010),
+                 ('DB_VLAN', 11)]
+sorted(list_of_tuple,key=lambda x: x[1])
+
+
+# function map 
+
+# Функция map применяет функцию к каждому элементу
+# последовательности и возвращает итератор с результатами.
+
+list_of_numbers = ['one','two','three','four','']
+map(str.upper, list_of_numbers)
+list(map(str.upper, list_of_numbers))
+
+
+list_of_str = ["1","2",'3']
+list(map(int, list_of_str))
+
+# map + lambda 
+
+vlans = [100,200,300,400,104,507]
+list(map(lambda x: "vlans {}".format(x), vlans))
+
+nums = [1, 2, 3, 4, 5]
+nums2 = [100, 200, 300, 400, 500]
+list(map(lambda x, y: x*y, nums, nums2))
+
+# list comperhention вместо map 
+
+list_of_numbers = ['one','two','three','four','']
+[num.upper() for num in list_of_numbers]
+
+
+list_of_str = [1,2,3,45,43,-1]
+[int(x) for x in list_of_str]
+
+# function filter 
+# Функция filter применяет функцию ко всем 
+# элементам последовательности и возвращает 
+# итератор с теми объектами, для которых функция вернула True.
+
+list_of_strings = ['one', 'two', 'list', '', 'dict', '100', '1', '50']
+filter(str.isdigit, list_of_strings)
+
+list(filter(str.isdigit, list_of_strings))
+
+list(filter(lambda x: x%2 == 0 , [10, 111, 102, 213, 314, 515,0]))
+
+list_of_word = ['one', 'two', 'list', '', 'dict']
+list(filter(lambda x: len(x) > 2, list_of_word))
+
+# list comperhention вместо filter 
+
+list_of_string = ['one', 'two', 'list', '', 'dict', '100', '1', '50']
+[w for w in list_of_string if w.isdigit()]
+
+nums = [10, 111, 102, 213, 314, 515]
+print([s for s in nums if s % 2 == 1])
+print([g for g in nums if g % 2 == 0])
+
